51b9a9d24f5d41f98ad27fc4b838550f521cedec
iPavka <ipavka@ya.ru>
2023-03-25 22:49:36 +0100

    hw11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -10,42 +10,78 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 * 3 - сделать стили в соответствии с дизайном
 * */
 
-function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+const minDistance = 10;
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+function HW11() {
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+  const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+  const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+
+  const change = (event: Event, newValue: number | number[], activeThumb: number) => {
+    if (!Array.isArray(newValue)) {
+      setValue1(newValue);
+      return;
     }
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setValue1(clamped);
+        setValue2(clamped + minDistance);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue2(clamped - minDistance);
+        setValue1(clamped);
+      }
+    } else {
+      setValue1(newValue[0] as number);
+      setValue2(newValue[1] as number);
+    }
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
+    // console.log(activeThumb)
+    // if (Array.isArray(newValue)) {
+    //   setValue1(newValue[0]);
+    //   setValue2(newValue[1]);
+    // } else {
+    //   setValue1(newValue);
+    // }
+    // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+  }
 
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
+  return (
+    <div id={'hw11'}>
+      <div className={s2.hwTitle}>Homework #11</div>
 
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
-            </div>
+      <div className={s2.hw}>
+        <div className={s.container}>
+          <div className={s.wrapper}>
+            <span id={'hw11-value'} className={s.number}>{value1}</span>
+            <SuperRange
+              id={'hw11-single-slider'}
+              value={value1}
+              onChange={change}
+              valueLabelDisplay="auto"
+              // сделать так чтоб value1 изменялось // пишет студент
+
+            />
+          </div>
+          <div className={s.wrapper}>
+            <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+            <SuperRange
+              id={'hw11-double-slider'}
+              value={[value1, value2]}
+              onChange={change}
+              getAriaLabel={() => 'double-slider'}
+              valueLabelDisplay="auto"
+              // сделать так чтоб value1/2 изменялось // пишет студент
+
+            />
+            <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default HW11
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        color: '#c47a0f',
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+    />
+  )
 }
 
 export default SuperRange
