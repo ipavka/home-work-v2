f7c2eab240ebef39fe19e04d7d1da27a9e615b30
iPavka <ipavka@ya.ru>
2023-03-25 09:11:06 +0100

    hw10_v1

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,12 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+  return (
+    <div className={s.loader}>
+      <div className={s.container}>
+        <div className={s.speedingWheel}></div>
+      </div>
+    </div>
+  )
+}
+
